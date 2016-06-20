e8c3d36631dd0e40bb7fb58ff32744144b1b8139
marcela_mpc <marcela_pcarvalho@hotmail.com>
2016-06-20 11:19:29 -0300

    trying to add a new javascript to the projects

---
+++ js/projects.js
@@ -0,0 +1,13 @@
+
+function callOtherProjects(modal_id){
+    if(modal_id == 2){
+        var imgproj2 = document.createElement("img");
+        imgproj2.src = "img/projects/{{ post.imgproj2 }}"
+    }
+}
+
+
+// <img src="img/projects/{{ post.imgproj2 }}" class="img-responsive img-centered" alt="{{ post.alt }}">
+// <p>{{ post.description2 }}</p>
+// <img src="img/projects/{{ post.imgproj3 }}" class="img-responsive img-centered" alt="{{ post.alt }}">
+// <p>{{ post.description3 }}</p>
